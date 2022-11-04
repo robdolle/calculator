021c2f4555e01bc6607877f35f4d37ff9b24e281
unknown <robin.dadolle@gmail.com>
2022-11-04 14:47:29 +0100

    a

--- src/test/java/robdolle/calculator/CalculatorApplicationTests.java
+++ src/test/java/robdolle/calculator/CalculatorApplicationTests.java
@@ -2,12 +2,16 @@ package robdolle.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 
 @SpringBootTest
 class CalculatorApplicationTests {
-
+	private CalculatorApplication calculator = new CalculatorApplication();
 	@Test
-	void contextLoads() {
+	public void testSum() {
+		assertEquals(5, calculator.sum(2, 3));
 	}
 
 }
