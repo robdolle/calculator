853315f8c726595e6428f4322f0ed6c33c3e935d
unknown <robin.dadolle@gmail.com>
2022-11-04 14:19:34 +0100

    a

--- src/main/java/robdolle/calculator/CalculatorApplication.java
+++ src/main/java/robdolle/calculator/CalculatorApplication.java
@@ -2,12 +2,15 @@ package robdolle.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
-@SpringBootApplication
+@Service
 public class CalculatorApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
+	public static void main(String[] args){
+	
 	}
-
+	
+	public int sum(int a, int b) {
+		return a + b;
+	}
 }
